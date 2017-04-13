fec5ab2aa2861599fd1974e060c94a5795c5c64a
adam-bates <200335831@student.georgianc.on.ca>
2017-04-13 16:02:50 -0400

    added event handlers gas buttons

--- src/com/group/gasstation/FrameSunoco.java
+++ src/com/group/gasstation/FrameSunoco.java
@@ -5,8 +5,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,8 +17,6 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 /**
  * This class provides GUI for user to pump the gas.
@@ -55,8 +52,15 @@ public class FrameSunoco extends JFrame {
     private final int FRAME_HEIGHT = 670;
     private final int FRAME_HEIGHT_SOUTH = 240;
     
+    // Pricing
+    private final double REGULAR_PRICE = 0.9905;
+    private final double PLUS_PRICE = 1.0905;
+    private final double SUPREME_PRICE = 1.1905;
+    
     // Preset Amount
     private int presetAmount = 0;
+    
+    private double gasLiters = 0, gasPrice = 0, gasPricePerLiter = 0;
     
     // true while pumping
     private boolean isPumping = false;
@@ -84,25 +88,25 @@ public class FrameSunoco extends JFrame {
         panelCenterSixth = new JPanel();
         // Labels
         labelSunoco = new JLabel("SUNOCO");
-        labelSaleA = new JLabel("1");
-        labelSaleB = new JLabel("3");
-        labelSaleC = new JLabel("5");
-        labelSaleD = new JLabel("9");
-        labelSaleE = new JLabel("9");
+        labelSaleA = new JLabel("0");
+        labelSaleB = new JLabel("0");
+        labelSaleC = new JLabel("0");
+        labelSaleD = new JLabel("0");
+        labelSaleE = new JLabel("0");
         labelSale = new JLabel("This Sale $");
         labelLiterA = new JLabel("0");
-        labelLiterB = new JLabel("9");
-        labelLiterC = new JLabel("5");
-        labelLiterD = new JLabel("9");
-        labelLiterE = new JLabel("9");
+        labelLiterB = new JLabel("0");
+        labelLiterC = new JLabel("0");
+        labelLiterD = new JLabel("0");
+        labelLiterE = new JLabel("0");
         labelLiter = new JLabel("Liters");
         labelPriceValue = new JLabel("0.00");
         labelPrice = new JLabel("Price Per Liter ¢");
         labelPreset = new JLabel("Preset Purchase Amount");
-        // TextFields
-        textFieldRegular = new JTextField("99.05");
-        textFieldPlus = new JTextField("109.05");
-        textFieldSupreme = new JTextField("119.05");
+        // TextFields (Gas grade prices)
+        textFieldRegular = new JTextField(String.format("%.2f", REGULAR_PRICE*100));
+        textFieldPlus = new JTextField(String.format("%.2f", PLUS_PRICE*100));
+        textFieldSupreme = new JTextField(String.format("%.2f", SUPREME_PRICE*100));
         // Buttons
         buttonRegular = new JButton("Regular");
         buttonPlus = new JButton("Plus");
@@ -186,6 +190,25 @@ public class FrameSunoco extends JFrame {
             presetAmount = sliderPreset.getValue();
             labelPreset.setText("Preset Purchase Amount" + (presetAmount == 0? "" : " $" + presetAmount));
         });
+        // Gas grade event handler
+        ActionListener gradeHandler = (e) -> {
+            JButton source = (JButton) e.getSource();
+            if(!buttonStart.isEnabled()) buttonStart.setEnabled(true);
+            
+            double ppl;
+            if(source == buttonRegular) ppl = REGULAR_PRICE;
+            else if(source == buttonPlus) ppl = PLUS_PRICE;
+            else ppl = SUPREME_PRICE;
+            
+            gasPricePerLiter = ppl;
+            labelPriceValue.setText(String.format("%.2f", ppl*100));
+        };
+        // add event handler to gas grade buttons
+        buttonRegular.addActionListener(gradeHandler);
+        buttonPlus.addActionListener(gradeHandler);
+        buttonSupreme.addActionListener(gradeHandler);
+        // handle start and exit buttons
+        buttonStart.setEnabled(false); // Can't pump until choose grade
         buttonStart.addActionListener((e)-> {
             // Handles pumping gas
             isPumping = !isPumping;
@@ -201,20 +224,31 @@ public class FrameSunoco extends JFrame {
                         // time trackers for sleep implementation
                         long now = System.currentTimeMillis();
                         long then = now;
-
+                        
                         while(isPumping)
                         {
                             now = System.currentTimeMillis();
-                            if(now - then > 500) // sleep for 500 milliseconds
+                            if(now - then > 100) // milliseconds to sleep
                             {
                                 then = now;
 
                                 // update gas amounts here
-                                System.out.println("GLUB");
+                                gasLiters += 0.01;
+                                gasPrice = gasLiters * gasPricePerLiter;
+                                
+                                updateGasPrice(gasPrice);
+                                updateGasLiters(gasLiters);
+                                
+                                System.out.printf("Cost ($): %.2f\nGas (L): %.2f\n\n",
+                                        gasPrice, gasLiters);
                             }
                         }
                     }
                 }).start();
+            }
+            else
+            {
+                buttonStart.setEnabled(false);
             }
         });
         // Button for exiting application
@@ -281,6 +315,84 @@ public class FrameSunoco extends JFrame {
         add(panelSouth, BorderLayout.SOUTH);
         setLocationRelativeTo(null); //Center the window
         setVisible(true); // show the window
+    }
+    
+    // Update required JLabels
+    private void updateGasPrice(double price)
+    {
+        /*
+            Turn price into a char array (digits)
+        
+            Ex: price = 203.67
+                -> digits = ['2', '0', '3', '6', '7']
+        */
+        price = Math.floor(price * 100) / 100;
+        StringBuilder sb = new StringBuilder(String.format("%.2f", price));
+        char[] digits = sb.deleteCharAt(sb.length() - 3).toString().toCharArray();
+        
+        // only updates JLabels needing updating
+        if(price >= 0.01)
+        {
+            labelSaleE.setText(String.valueOf(digits[digits.length - 1]));
+            
+            if(price >= 0.1)
+            {
+                labelSaleD.setText(String.valueOf(digits[digits.length - 2]));
+                
+                if(price >= 1)
+                {
+                    labelSaleC.setText(String.valueOf(digits[digits.length - 3]));
+                    
+                    if(price >= 10)
+                    {
+                        labelSaleB.setText(String.valueOf(digits[digits.length - 4]));
+                        
+                        if(price >= 100)
+                        {
+                            labelSaleA.setText(String.valueOf(digits[digits.length - 5]));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private void updateGasLiters(double liters)
+    {
+        /*
+            Turn liters into a char array (digits)
+        
+            Ex: liters = 203.67
+                -> digits = ['2', '0', '3', '6', '7']
+        */
+        liters = Math.floor(liters * 100) / 100;
+        StringBuilder sb = new StringBuilder(String.format("%.2f", liters));
+        char[] digits = sb.deleteCharAt(sb.length() - 3).toString().toCharArray();
+        
+        // only updates JLabels needing updating
+        if(liters >= 0.01)
+        {
+            labelLiterE.setText(String.valueOf(digits[digits.length - 1]));
+            
+            if(liters >= 0.1)
+            {
+                labelLiterD.setText(String.valueOf(digits[digits.length - 2]));
+                
+                if(liters >= 1)
+                {
+                    labelLiterC.setText(String.valueOf(digits[digits.length - 3]));
+                    
+                    if(liters >= 10)
+                    {
+                        labelLiterB.setText(String.valueOf(digits[digits.length - 4]));
+                        
+                        if(liters >= 100)
+                        {
+                            labelLiterA.setText(String.valueOf(digits[digits.length - 5]));
+                        }
+                    }
+                }
+            }
+        }
     }
     
     // Start application
